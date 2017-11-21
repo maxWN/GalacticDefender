784f92bc345e5324df4a2f3d23d0b988423e0a41
Max <maxnakel@gmail.com>
2017-11-20 23:41:47 -0500

    Replaced Main.js with MainState.js. Applied proper TS syntax to MainState.ts.

--- GalacticDefender/MainState.js
+++ GalacticDefender/MainState.js
@@ -6,7 +6,9 @@ var FinalFrontier;
 (function (FinalFrontier) {
     var MainState = (function () {
         function MainState() {
-            this.game = new Phaser.Game(800, 600, Phaser.CANVAS, 'object', { preload: this.preload, create: this.create });
+            this.BeginState = { preload: this.preload, create: this.create, update: this.update };
+            this.game = new Phaser.Game(800, 600, Phaser.CANVAS, 'object');
+            this.begin();
         }
         //var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'object');
         //var BeginState = {
@@ -17,11 +19,12 @@ var FinalFrontier;
             }
         };
         MainState.prototype.preload = function () {
+            console.log("Pre-load files before execution.");
             //Load all of our assets before the game starts
             //Graphics
             this.game.load.image('background', '/Graphics/space.png');
             //Music
-            this.game.load.audio('title_music', '/Music/02 Paranoid Android.mp3');
+            //this.game.load.audio('title_music', '/Music/02 Paranoid Android.mp3');
             //Sprites
             this.game.load.image('player_one', '/Graphics/redRazor.png');
             this.game.load.image('commonE', '/Graphics/scytharWarship.png');
@@ -32,23 +35,27 @@ var FinalFrontier;
             this.game.load.image('game_title', '/Graphics/game_title.png');
         };
         MainState.prototype.create = function () {
-            var fixture = this.game.add.tileSprite(0, 0, 800, 600, 'background');
-            this.x = fixture;
+            this.fixture = this.game.add.tileSprite(0, 0, 800, 600, 'background');
+            this.x = this.fixture;
             //this.GameObjectsAlias(fixture);
             var set = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player_one');
             set.anchor.setTo(0, 2);
             var set2 = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'game_title');
             set2.anchor.setTo(0.5, 0.5);
-            var title_track = this.game.add.audio('title_music');
-            title_track.volume = 80;
-            title_track.loop = true;
-            title_track.play();
+            //var title_track = this.game.add.audio('title_music');
+            //title_track.volume = 80;
+            //title_track.loop = true;
+            //title_track.play();
             this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         };
         MainState.prototype.update = function () {
             //var z = this.ReturnAlias();
-            //this.fixture.tilePosition.y += 2;
-            this.x.tilePosition.y += 2;
+            this.fixture.tilePosition.y += 2;
+            //this.x.tilePosition.y += 2;
+        };
+        MainState.prototype.begin = function () {
+            this.game.state.add('BeginState', this.BeginState);
+            this.game.state.start('BeginState');
         };
         return MainState;
     }());
--- GalacticDefender/MainState.ts
+++ GalacticDefender/MainState.ts
@@ -6,10 +6,11 @@
 module FinalFrontier {
     export class MainState {
 
-        game: Phaser.Game;
+        public game: Phaser.Game;
 
         constructor() {
-            this.game = new Phaser.Game(800, 600, Phaser.CANVAS, 'object', { preload: this.preload, create: this.create });
+            this.game = new Phaser.Game(800, 600, Phaser.CANVAS, 'object');
+            this.begin();
         }
 
     //var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'object');
@@ -23,16 +24,18 @@ module FinalFrontier {
             }
         } 
 
-        x: any;
+        public x: any;
+        public fixture: any;
         
-        preload() {
+        public preload() {
+            console.log("Pre-load files before execution.");
             //Load all of our assets before the game starts
 
             //Graphics
             this.game.load.image('background', '/Graphics/space.png');
 
             //Music
-            this.game.load.audio('title_music', '/Music/02 Paranoid Android.mp3');
+            //this.game.load.audio('title_music', '/Music/02 Paranoid Android.mp3');
 
             //Sprites
             this.game.load.image('player_one', '/Graphics/redRazor.png');
@@ -44,34 +47,37 @@ module FinalFrontier {
             this.game.load.image('game_title', '/Graphics/game_title.png');
         }
 
-        create() { //create states
-            var fixture = this.game.add.tileSprite(0, 0, 800, 600, 'background');
-            this.x = fixture;
+        public create() { //create states
+            this.fixture = this.game.add.tileSprite(0, 0, 800, 600, 'background');
+            this.x = this.fixture;
             //this.GameObjectsAlias(fixture);
 
-            var set = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player_one');
+            let set = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player_one');
             set.anchor.setTo(0, 2);
 
-            var set2 = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'game_title');
+            let set2 = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'game_title');
             set2.anchor.setTo(0.5, 0.5);
 
-            var title_track = this.game.add.audio('title_music');
-            title_track.volume = 80;
-            title_track.loop = true;
-            title_track.play();
+            //var title_track = this.game.add.audio('title_music');
+            //title_track.volume = 80;
+            //title_track.loop = true;
+            //title_track.play();
 
             this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         }
 
-        update() {
+        public update() {
             //var z = this.ReturnAlias();
-            //this.fixture.tilePosition.y += 2;
-            this.x.tilePosition.y += 2;
+            this.fixture.tilePosition.y += 2;
+            //this.x.tilePosition.y += 2;
         }
 
-        //game.state.add('BeginState', BeginState);
-        //game.state.start('BeginState');
+        public BeginState = { preload: this.preload, create: this.create, update: this.update }
 
+        public begin() {
+        this.game.state.add('BeginState', this.BeginState);
+        this.game.state.start('BeginState');
+        }
     }
 
 }
