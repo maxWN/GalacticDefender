8c17a5294aa63a94be402aacdc58462b32663dd1
Max <maxnakel@gmail.com>
2018-01-31 21:56:11 -0500

    Added player directional controls.

--- GalacticDefender/States/LevelOne.js
+++ GalacticDefender/States/LevelOne.js
@@ -13,11 +13,17 @@ var FirstLevel;
         __extends(LevelOne, _super);
         function LevelOne() {
             _super.call(this);
+            //player attributes
+            this.FLIGHT_SPEED = 0;
         }
         LevelOne.prototype.create = function () {
             this.fixture = this.game.add.tileSprite(0, 0, 800, 600, 'background');
             this.player_one = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player_one');
             this.player_one.anchor.setTo(.5, -2);
+            this.FORWARD_KEY = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
+            this.BACKWARD_KEY = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+            this.RIGHT_KEY = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+            this.LEFT_KEY = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
         };
         return LevelOne;
     }(Phaser.State));
--- GalacticDefender/States/LevelOne.ts
+++ GalacticDefender/States/LevelOne.ts
@@ -11,6 +11,15 @@ module FirstLevel {
         public fixture: Phaser.TileSprite;
         public player_one: Phaser.Sprite;
 
+        //player controls
+        RIGHT_KEY: Phaser.Key;
+        LEFT_KEY: Phaser.Key;
+        FORWARD_KEY: Phaser.Key;
+        BACKWARD_KEY: Phaser.Key;
+
+        //player attributes
+        FLIGHT_SPEED: number = 0;
+
         constructor() {
             super();
         }
@@ -21,6 +30,11 @@ module FirstLevel {
 
             this.player_one = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player_one');
             this.player_one.anchor.setTo(.5, -2);
+
+            this.FORWARD_KEY = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
+            this.BACKWARD_KEY = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+            this.RIGHT_KEY = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+            this.LEFT_KEY = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
 
         }
 
